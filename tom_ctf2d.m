9f1216160bbaba2baffc744f6c79815f8d118a73
Dimitry Tegunov <tegunov@gmail.com>
2015-08-16 14:48:07 +0200

    CTF methods for visualization.

--- tom_ctf2d.m
+++ tom_ctf2d.m
@@ -1,15 +1,19 @@
-function [ ctf, acurve, pcurve ] = tom_ctf1d( length, pixelsize, voltage, cs, defocus, amplitude, phaseshift, bfactor )
+function [ ctf, acurve, pcurve ] = tom_ctf2d( length, pixelsize, voltage, cs, defocus, astigmatism, angle, amplitude, phaseshift, bfactor )
 
 ny = 0.5 / pixelsize;
 voltagerel = voltage * (1 + voltage / 1022000);
 lambda = sqrt(150.4 / voltagerel) * 1e-10;
 lambda2 = lambda * 2;
 
-points = 0:length-1;
+[x, y] = ndgrid(-length:length-1, -length:length-1);
+alpha = atan2(y, x);
+points = sqrt(x.^2+y.^2);
 points = points./(length).*ny;
 k2 = points.^2;
 term1 = lambda.^3.*cs.*k2.^2;
 
+astigmatism = astigmatism / 2;
+defocus = defocus + astigmatism.*cos((alpha - angle).*2);
 w = pi./2.*(term1 + lambda2.*defocus.*k2) - phaseshift;
 
 acurve = cos(w).*amplitude;
